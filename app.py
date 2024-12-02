457c330b05827441926eec32ea9b20e18251d079
Олеся Занозина <zanozina.o@gmail.com>
2024-12-02 20:03:14 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app = Flask (__name__)
 
 @app.route("/")
 def start ():
-    return "web-сервер на flask"
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Занозина Олеся Евгеньевна, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Занозина Олеся, ФБИ-23, 2024
+        </footer>
+    </body>
+</htnl>
+"""
