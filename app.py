89f3102522c741a586bb48b5fec0c0811e011e59
Занозина Олеся <zanozina.o@gmail.com>
2024-12-26 05:13:07 +0700

    Код ответа 201

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, redirect
 app = Flask (__name__)
 
 @app.route("/")
@@ -65,3 +65,18 @@ def counter():
 </html>
 ''' 
 
+@app.route('/info')
+def info():
+    return redirect('/author')
+
+@app.route('/lab1/created')
+def created():
+    return '''
+<!doctype html>
+<html>
+    <body>
+        <h1>Создано успешно</h1>
+        <div><i>что-то создано...</i></div>
+    </body>
+</html>
+''', 201
