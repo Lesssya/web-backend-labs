921ff9d7bcbdb354a03b4a31ce136d5029dbd2a2
Олеся Занозина <zanozina.o@gmail.com>
2025-01-19 23:27:21 +0700

    снова правки

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,12 +12,10 @@ def start():
             <header>
                 НГТУ, ФБ, Лабораторная работа 1
             </header>
-
             <h1>web-сервер на flask</h1>
-            <a href="/lab1/author">author</a>  #изменен роут /author на /lab1/author
-            <footer>
                 &copy; Занозина Олеся, ФБИ-23, 2024
             </footer>
         </body>
     </html>
     """
+
