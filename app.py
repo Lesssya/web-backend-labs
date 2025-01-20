40d521d93a687dbac06616492bdfacaad75962af
Олеся Занозина <zanozina.o@gmail.com>
2025-01-20 17:37:42 +0700

    Заготовка шаблона

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route("/index")
 def start():
     return redirect("/menu", code=302)
+
+@app.route('/lab2/example')
+def example():
+    return render_template('example.html')
+
     
 @app.route("/lab1/oak")
 def oak():
