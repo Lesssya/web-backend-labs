2ca8dce41986d25de7a14419a3ac781b66c3ee69
Олеся Занозина <zanozina.o@gmail.com>
2025-01-24 22:59:53 +0700

    Книги

--- app.py
+++ app.py
@@ -17,9 +17,21 @@ def example():
         {'name': 'мандарины', 'price': 95}, 
         {'name': 'манго', 'price': 321},
     ]
+    book = [
+        {'author': 'Габриэль Гарсиа Маркес', 'name_book': 'Сто лет одиночества', 'style': 'Роман', 'num_page': '480'},
+        {'author': 'Рэй Брэдбери', 'name_book': '451 градус по Фаренгейту', 'style': 'Антиутопия', 'num_page': '200'},
+        {'author': 'Михаил Булгаков', 'name_book': 'Мастер и Маргарита', 'style': 'Роман', 'num_page': '470'},
+        {'author': 'Джон Эрнст Стейнбек', 'name_book': 'Гроздья гнева', 'style': 'Реалистический роман', 'num_page': '590'},
+        {'author': 'Эрих Мария Ремарк', 'name_book': 'На западном фронте без перемен', 'style': 'Антивоенный роман', 'num_page': '200'},
+        {'author': 'Колин Маккалоу', 'name_book': 'Поющие в терновнике', 'style': 'Семейная сага', 'num_page': '820'},
+        {'author': 'Михаил Шолохов', 'name_book': 'Тихий дон', 'style': 'Роман-эпопея', 'num_page': '1810'},
+        {'author': 'Лев Толстой', 'name_book': 'Война и Мир', 'style': 'Роман-эпопея', 'num_page': '1979'},
+        {'author': 'Виктор Мари Гюго', 'name_book': 'Отверженные', 'style': 'Трагедия', 'num_page': '1871'},
+        {'author': 'Антуан де Сент-Экзюпери', 'name_book': 'Маленький принц', 'style': 'Философская сказка', 'num_page': '78'},
+    ]
     return render_template ('example.html', num_lab=num_lab, 
                             name=name, group=group, course=course,
-                            fruits=fruits)
+                            fruits=fruits, book=book)
 
     
 @app.route("/lab1/oak")
