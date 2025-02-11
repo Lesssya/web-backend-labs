43e603a2bb68c9dc0c19abec6c6451df8b964dec
Олеся Занозина <zanozina.o@gmail.com>
2025-02-11 22:39:49 +0700

    валидация

--- lab4.py
+++ lab4.py
@@ -112,14 +112,16 @@ def tree():
     operation = request.form.get('operation')
     
     if operation == 'cut':
-        tree_count -= 1
+        if tree_count > 0:
+            tree_count -= 1
     elif operation == 'plant':
-        tree_count += 1
+        if tree_count < 10:
+            tree_count += 1
     
     
 
     #return render_template('lab4/tree.html', tree_count=tree_count)
-    return redirect ('lab4/tree')
+    return redirect('/lab4/tree')
 
 
 
