52e8511ea203415dfbaf5b83ead1f89b1958694f
Олеся Занозина <zanozina.o@gmail.com>
2025-02-13 00:02:54 +0700

    Итог Лабораторная работ 6

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
+from lab6 import lab6
 from RGZ import RGZ
 
 app = Flask(__name__)
@@ -18,6 +19,7 @@ app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
+app.register_blueprint(lab6)
 app.register_blueprint(RGZ)
 
 
@@ -50,6 +52,7 @@ def menu():
                     <li><a href="/lab3">Третья лабораторная</a></li>
                     <li><a href="/lab4">Четвертая лабораторная</a></li>
                     <li><a href="/lab5">Пятая лабораторная</a></li>
+                    <li><a href="/lab6">Шестая лабораторная</a></li>
                 </ul>
             </nav>
 
